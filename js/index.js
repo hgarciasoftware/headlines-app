fd6b47b213ea5473dd3085251d44ec5a44313807
hgarciasoftware <93831791+hgarciasoftware@users.noreply.github.com>
2026-06-02 17:59:01 -0400

    Display an error message on fetch errors

--- js/index.js
+++ js/index.js
@@ -3,8 +3,23 @@ document.querySelector('.js-refresh-app').addEventListener('click', () => {
   const category = document.querySelector('#category').value;
 
   fetch(`https://newsapi-proxy.herokuapp.com?country=${country}&category=${category}`)
-    .then(res => res.json())
-    .then(data => renderArticleData(data.articles));
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`HTTP ${res.status} fetching ${res.url}`);
+      }
+
+      return res.json();
+    })
+    .then(data => renderArticleData(data.articles))
+    .catch(e => {
+      const app = document.querySelector('.js-app');
+      const textContent = e instanceof TypeError
+        ? `error in fetch: ${e}`
+        : e.message;
+
+      app.textContent = textContent;
+      app.style.color = 'darkred';
+    })
 });
 
 document.querySelector('.js-refresh-app').click();
