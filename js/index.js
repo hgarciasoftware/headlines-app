ebf1d7c81669fe1d8ffe85e367d6d3691176a6a0
hgarciasoftware <93831791+hgarciasoftware@users.noreply.github.com>
2026-06-02 18:18:46 -0400

    Replace News API with Spaceflight News API
    * News API's free tier only enables CORS for localhost
    * This made it necessary to use a CORS proxy (Heroku)
    * Heroku removed its free tier in November 22
    * Rather than find another proxy host, replace News API with an API that supports CORS

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.querySelector('.js-refresh-app').addEventListener('click', () => {
   const country = document.querySelector('#country').value;
   const category = document.querySelector('#category').value;
 
-  fetch(`https://newsapi-proxy.herokuapp.com?country=${country}&category=${category}`)
+  fetch(`https://api.spaceflightnewsapi.net/v4/articles/`)
     .then(res => res.json())
     .then(data => renderArticleData(data.articles));
 });
