461d57b64647ba736365f99a8351a3f683ba45fa
hgarciasoftware <93831791+hgarciasoftware@users.noreply.github.com>
2026-06-03 12:24:55 -0400

    Update footer and page description to support new API

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 document.querySelector('.js-refresh-app').addEventListener('click', () => {
-  const country = document.querySelector('#country').value;
-  const category = document.querySelector('#category').value;
+  // const country = document.querySelector('#country').value;
+  const endpoint = document.querySelector('#endpoint').value;
 
-  fetch(`https://api.spaceflightnewsapi.net/v4/articles/`)
+  fetch(`https://api.spaceflightnewsapi.net/v4/${endpoint}/`)
     .then(res => res.json())
     .then(data => renderArticleData(data.results));
 });
